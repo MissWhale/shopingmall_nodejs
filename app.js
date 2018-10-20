58f26e16f9b236c3f413b9ffce2dc4da69d294f4
MissWhale <doden5180@gmail.com>
2018-10-20 17:34:08 +0900

    editor

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
         var app=express();
         var mysql=require('mysql');
         var session=require('express-session');
+        var multer=require('multer');
+        var fs=require('fs');
+        var upload=multer({storage: multer.diskStorage({
+                destination: function (req, file, cb) {
+                  cb(null, __dirname+'/dbimg/');
+                },
+                filename: function (req, file, cb) {
+                  cb(null, Date.now()+"-"+file.originalname);
+                }
+        }),});
         var mss=require('express-mysql-session')(session);
         { //mysql
         // var option={
@@ -26,14 +36,16 @@
                 port:3306,
                 user:'root',
                 password:'tmvlflt1234',
-                database:'Mok2'
+                database:'Mok2',
+                charset:'utf8'
         }
         var sstore=new mss(option);
         var con=mysql.createConnection({
                 host:'114.71.137.109',
                 user:'root',
                 password:'tmvlflt1234',
-                database:'Mok2'
+                database:'Mok2',
+                charset:'utf8'
         })
         con.connect();
         app.locals.pretty=true;
@@ -45,6 +57,7 @@
         app.use("/img", express.static(__dirname + '/img'));
         app.use(express.static(__dirname+'/js'));
         app.use("/js", express.static(__dirname + '/js'));
+        app.use("/dbimg", express.static(__dirname + '/dbimg'));
         app.use(bodyParser.urlencoded({extended:false}))
         app.use(session({
                 secret:'test',
@@ -133,12 +146,70 @@
                 }
         })
         app.get("/product",function(req,res){
-                if(req.session.displayname){
-                        var dname=req.session.displayname;
-                        res.render('product',{name:dname,id:req.session.user});
-                }else{
-                        res.render('product');
-                }
+                var sql="select *  from product";
+                con.query(sql,function(err,result){
+                        if(err) console.log(err);
+                        else{
+                                if(req.session.displayname){
+                                        var dname=req.session.displayname;
+                                        res.render('product',{name:dname,id:req.session.user,result:result});
+                                }else{
+                                        res.render('product',{result:result});
+                                }
+                        }
+                })
+        })
+        app.post("/productdelete",function(req,res){ //상품 삭제
+                var num=req.body.num;
+                var sql="delete from product where num=?";
+                con.query(sql,num,function(err,result){
+                        if(err) console.log(err);
+                        else{
+                                if(result.affectedRows){
+                                        var sql="select simg,bimg from productimg where num=?";
+                                        con.query(sql,num,function(err,result){
+                                                if(err) console.log(err);
+                                                else{
+                                                        var simg=result[0].simg;
+                                                        var bimg=result[0].bimg;
+                                                        fs.exists(simg,function(ex){ //썸네일 삭제
+                                                                if(ex){
+                                                                        fs.unlink(simg,function(err){
+                                                                                if(err) console.log(err);
+                                                        })}})
+                                                        fs.exists(bimg,function(ex){ //이미지 삭제
+                                                                if(ex){
+                                                                        fs.unlink(bimg,function(err){
+                                                                                if(err) console.log(err);
+                                                        })}})
+                                                        var sql="delete from productimg where num=? " //사진 db제거
+                                                        con.query(sql,num,function(err,result){
+                                                                if(err) console.log(err);
+                                                                else{
+                                                                        if(result.affectedRows) res.send(true);
+                                                                        else res.send(false);
+                                                                }
+                                                        })
+                                                }
+                                        })
+                                }else res.send(false);
+                        }
+                })
+        })
+        app.get(["/productmodify",'/productmodify?:num'],function(req,res){ //상품수정
+                var num=req.query.num;
+                var sql="select * from product natural join productimg where num=?";
+                con.query(sql,num,function(err,result){
+                        if(err) console.log(err);
+                        else{
+                                if(req.session.displayname){
+                                        var dname=req.session.displayname;
+                                        res.render('productmodify',{name:dname,id:req.session.user,result:result[0]});
+                                }else{
+                                        res.render('productmodify');
+                                }
+                        }
+                })
         })
         app.get("/productupload",function(req,res){
                 if(req.session.displayname){
@@ -148,7 +219,7 @@
                         res.render('productupload');
                 }
         })
-        app.post("/productupload",function(req,res){
+        app.post("/productupload",function(req,res){ //판매등록
                 var pro={
                         name:req.body.name,
                         kind:req.body.kind,
@@ -157,16 +228,69 @@
                         count:req.body.count,
                         price:req.body.price,
                         text:req.body.text,
-                        simg:req.body.simg,
-                        bimg:req.body.bimg
                 }
-                var sql='insert into product(pkind,pnum,name,comp,count,price,ptext,simg,bimg) values(?,?,?,?,?,?,?,?,?)';
-                con.query(sql,[pro.kind,pro.num,pro.name,pro.comp,pro.count,pro.price,pro.text,pro.simg,pro.bimg],function(err,result){
+                var sql='insert into product(pkind,pnum,name,comp,count,price,ptext) values(?,?,?,?,?,?,?)';
+                con.query(sql,[pro.kind,pro.num,pro.name,pro.comp,pro.count,pro.price,pro.text],function(err,result){
                         if(err) console.log(err);
                         else{
                                 if(result.affectedRows){
-                                        res.render('product');
+                                        res.redirect('/product');
                                 }
+                        }
+                })
+        })
+        app.post("/simgdb",upload.single('simg'),function(req,res){ //썸네일업로드
+                var file={
+                        path:req.file.path,
+                        origin:req.file.originalname,
+                        name:req.file.filename
+                }
+                var num=req.body.no;
+                console.log(req);
+                // var sql='insert into productimg(simg,simgorigin,simgname,num) values(?,?,?,?)';
+                // con.query(sql,[file.path,file.origin,file.name,num],function(err,result){
+                //         if(err) console.log(err);
+                //         else{
+                //                 if(result.affectedRows){
+                //                         res.send("true");
+                //                 }else res.send("false");
+                //         }
+                // })
+        })
+        app.post("/bimgdb",upload.single('bimg'),function(req,res){ //이미지업로드
+                var file={
+                        path:req.file.path,
+                        origin:req.file.originalname,
+                        name:req.file.filename
+                }
+                var num=req.body.bno;
+                var sql="select count(*) as result from productimg where num=?"
+                con.query(sql,num,function(err,result){
+                        if(result[0].result){
+                                var sql="update productimg set bimg=?,bimgorigin=?,bimgname=? where num=?"
+                                con.query(sql,[file.path,file.origin,file.name,num],function(err,result){
+                                        if(err) console.log(err);
+                                        else res.send(true);
+                                })
+                        }else{
+                                var sql='insert into productimg(bimg,bimgorigin,bimgname,num) values(?,?)';
+                                con.query(sql,[file.path,file.origin,file.name,num],function(err,result){
+                                        if(err) console.log(err);
+                                        else{
+                                                if(result.affectedRows){
+                                                        res.send("true");
+                                                }else res.send("false");
+                                        }
+                                })
+                        }
+                })
+        })
+        app.post("/getnum",function(req,res){ //Auto_increment Get
+                var sql="SHOW TABLE STATUS LIKE 'product'";
+                con.query(sql,function(err,result){
+                        if(err) console.log(err);
+                        else{
+                                res.send({num:result[0].Auto_increment});
                         }
                 })
         })
@@ -260,16 +384,16 @@
                         pw:req.body.pw,
                         email:req.body.email,
                         name:req.body.name,
-                        location:req.body.location,
-                        birthday:req.body.birthday
+                        birthday:req.body.birth,
+                        phone:req.body.phone
                 }
-                var sql='insert into login(id,pw,email,name,location,birthday,status) values(?,?,?,?,?,?,1)';
-                con.query(sql,[user.id,user.pw,user.email,user.name,user.location,user.birthday],function(err,result){
+                var sql='insert into login(id,pw,email,name,birth,phone,status) values(?,?,?,?,?,?,1)';
+                con.query(sql,[user.id,user.pw,user.email,user.name,user.birthday,user.phone],function(err,result){
                         if(err) console.log(err);
                         else{
                                 var ok=result.affectedRows;
                                 if(ok==1){
-                                        res.send('<script>alert("회원가입에 성공하셧습니다.");location.href="/login"birthday;</script>');
+                                        res.send('<script>alert("회원가입에 성공하셧습니다.");location.href="/login"</script>');
                                 }else{
                                         res.send('<script>alert("회원가입을 실패하셨습니다.");history.back();</script>',{result:ok});
                                 }
